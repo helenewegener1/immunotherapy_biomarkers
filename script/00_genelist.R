0644f251b3f8098b14597d2e671cdfb57026ded5
helenewegener1 <helene.wegener@gmail.com>
2025-09-19 15:07:18 +0200

    adding signifinder biomarkers to geneset

--- script/00_genelist.R
+++ script/00_genelist.R
@@ -3,6 +3,8 @@ setwd('~/Documents/projects/project_DGD/cancer_sample_normalization/')
 library(tidyverse)
 library(glue)
 library(biomaRt)
+library(signifinder)
+library(dplyr)
 
 ##################################### TGEP ##################################### 
 # Gene list 
@@ -27,11 +29,62 @@ mapping_ensembl_to_symbol <- getBM(
 # Find the ensembl mapping of the gene list 
 TGEP_ensembl <- mapping_ensembl_to_symbol %>% filter(hgnc_symbol %in% TGEP)
 
+######################## Find gene sets on signifinder ######################### 
+
+availableSignatures(description = FALSE) %>% view()
+availableSignatures() 
+
+# sysdata.rda downlaoded from signifinder github: https://github.com/CaluraLab/signifinder/blob/main/R/sysdata.rda
+load('~/Downloads/sysdata.rda')
+
+# Not sure how to filter these
+EMT_Mak %>% head()
+EMT_Mak$class %>% table()
+
+EMT_Thompson %>% head()
+EMT_Thompson$class %>% table()
+
+PassON_Du %>% head()
+PassON_Du$class %>% table()
+
+Hypoxia_Buffa_genes <- Hypoxia_Buffa %>% dplyr::select(SYMBOL) %>% unlist()
+ImmuneCyt_Rooney_genes <- ImmuneCyt_Rooney %>% dplyr::select(SYMBOL) %>% unlist()
+Tinflam_Ayers_genes <- Tinflam_Ayers %>% filter(class == 'TInflam') %>% dplyr::select(SYMBOL) %>% unlist() 
+Tinflam_Thompson_genes <- Tinflam_Thompson %>% dplyr::select(SYMBOL) %>% unlist()
+TLS_Cabrita_genes <- TLS_Cabrita %>% dplyr::select(SYMBOL) %>% unlist()
+CIN_Carter_25_genes <- CIN_Carter %>% filter(class == 'CIN25') %>% dplyr::select(SYMBOL) %>% unlist()
+CIN_Carter_70_genes <- CIN_Carter %>% filter(class == 'CIN70') %>% dplyr::select(SYMBOL) %>% unlist()
+
+ECM_Chakravarthy_up_genes <- ECM_Chakravarthy %>% filter(class == 'ECMup') %>% dplyr::select(SYMBOL) %>% unlist()
+ECM_Chakravarthy_down_genes <- ECM_Chakravarthy %>% filter(class == 'ECMdown') %>% dplyr::select(SYMBOL) %>% unlist()
+VEGF_Hu_genes <- VEGF_Hu %>% dplyr::select(SYMBOL) %>% unlist()
+
 ################################################################################
 
+################# Translate gene lists from SYMBOL to ensembl ################## 
 
+gene_lists_symbol <- list(
+  Hypoxia_Buffa_genes, ImmuneCyt_Rooney_genes, Tinflam_Ayers_genes,
+  Tinflam_Thompson_genes, TLS_Cabrita_genes, CIN_Carter_25_genes,
+  CIN_Carter_70_genes, ECM_Chakravarthy_up_genes, ECM_Chakravarthy_down_genes,
+  VEGF_Hu_genes
+)
+
+names(gene_lists_symbol) <- c(
+  'Hypoxia_Buffa', 'ImmuneCyt_Rooney', 'Tinflam_Ayers',
+  'Tinflam_Thompson', 'TLS_Cabrita', 'CIN_Carter_25',
+  'CIN_Carter_70', 'ECM_Chakravarthy_up', 'ECM_Chakravarthy_down',
+  'VEGF_Hu'
+)
+
+# Find the ensembl mapping of the gene list 
+gene_lists_symbol_ensembl <- lapply(gene_lists_symbol, function(x) mapping_ensembl_to_symbol %>% filter(hgnc_symbol %in% x))
 
 # Use the ensembl IDs in final geneset since those are the IDs of the data
-geneset <- list(TGEP = TGEP_ensembl$ensembl_gene_id)
 
+geneset <- lapply(gene_lists_symbol_ensembl, function(x) x[['ensembl_gene_id']])
+
+################################################################################
+
+# Export genesets
 saveRDS(geneset, 'rds/geneset.rds')
